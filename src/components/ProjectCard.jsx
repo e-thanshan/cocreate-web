df577517b6eb9298f9d2db9ca347c40f19513586
Ethan Shan <shane1@rose-hulman.edu>
2023-11-01 00:26:04 -0400

    datamap

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
-const ProjectCard = () => {
+const ProjectCard = ({name}) => {
     return (
         <div className="overflow-hidden rounded-lg bg-white shadow-lg">
-            <div className="px-4 py-5 sm:p-6">{"Project Name"}</div>
+            <div className="px-4 py-5 sm:p-6">{name}</div>
         </div>
     );
 }
--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar"
 import ProjectCard from "../components/ProjectCard"
 
 const ExplorePage = () => {
+    const data = ["Project1", "Project2", "Project3", "Project4", "Project5"];
     return (
         <div>
             <Navbar />
@@ -9,13 +10,13 @@ const ExplorePage = () => {
             <h1 className="text-5xl pt-20 ">Explore</h1>
             <h1 className="text-2xl">Discover what the world has to offer.</h1>
             <div className="grid grid-cols-3 gap-3">
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
+                {data.map(v => {
+                    return (
+                        <ProjectCard name={v}/>
+                    );
+                })}
             </div>
+            <div>hi</div>
             
             </div>
         </div>
